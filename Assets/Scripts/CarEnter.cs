c5de9eda026d46684bb1688f6c3742edc50dc386
istrk33 <ilkerstrk33@gmail.com>
2023-02-05 15:26:05 +0100

    commit to go on bp pc

---
+++ Assets/Scripts/CarEnter.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityStandardAssets.Vehicles.Cars;
+
+
+public class CarEnter : MonoBehaviour
+{
+    public GameObject vehicleCam;
+    public GameObject thePlayer;
+    public GameObject liveVehicle;
+    public bool canEnter = false;
+    public GameObject exitTrig;
+    // public GameObject car;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(canEnter == true) 
+        
+        {
+            if(Input.GetKeyDown(KeyCode.E))
+            {
+                //this.gameObject.GetComponent<BoxCollider>().enabled = false;
+                vehicleCam.SetActive(true);
+                thePlayer.SetActive(false);
+            
+                liveVehicle.GetComponent<WheelController>().enabled = true;
+                /*liveVehicle.GetComponent<PlaneUserControll>().enabled = true;
+                liveVehicle.GetComponent<PlaneAudio>().enabled = true;*/
+                canEnter = false;
+                thePlayer.transform.parent = this.gameObject.transform; // attach the player to the current object
+                StartCoroutine(ExitTrigger());
+            }
+        }
+        
+    }
+
+    void OnTriggerEnter(Collider other) 
+    {
+        if (other.tag == "Player")
+        {
+           canEnter = true; 
+        }
+        
+    }
+
+    void OnTriggerExit(Collider other) 
+    {
+        canEnter = false;
+    }
+
+    IEnumerator ExitTrigger() 
+    {
+        yield return new WaitForSeconds(0.5f);
+        exitTrig.SetActive(true);
+        // planeHUD.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/WheelController.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+public class WheelController : MonoBehaviour
+{
+    [SerializeField] WheelCollider frontRight;
+    [SerializeField] WheelCollider frontLeft;
+    [SerializeField] WheelCollider backRight;
+    [SerializeField] WheelCollider backLeft;
+
+    public float acceleration = 500f;
+    public float breakingForce = 300f;
+    public float maxTurnAngle = 15f;
+
+    private float currentAcceleration = 0f;
+    private float currentBrakeForce = 0f;
+    private float currentTurnAngle = 0f;
+
+    private void FixedUpdate()
+    {
+        currentAcceleration = acceleration * Input.GetAxis("Vertical");
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            currentBrakeForce = breakingForce;
+        }
+        else
+        {
+            currentBrakeForce = 0f;
+        }
+
+        frontRight.motorTorque = currentAcceleration;
+        frontLeft.motorTorque = currentAcceleration;
+
+        frontRight.brakeTorque = currentBrakeForce;
+        frontLeft.brakeTorque = currentBrakeForce;
+        backRight.brakeTorque = currentBrakeForce;
+        backLeft.brakeTorque = currentBrakeForce;
+
+        currentTurnAngle = maxTurnAngle * Input.GetAxis("Horizontal");
+        frontLeft.steerAngle = currentTurnAngle;
+        frontRight.steerAngle = currentTurnAngle;
+    }
+
+    public void setCurrentAcceleration()
+    {
+        currentAcceleration = 0;
+        // frontRight.rpm = 0;
+        // frontLeft.rpm = 0;
+        frontRight.brakeTorque = Mathf.Infinity;
+        frontLeft.brakeTorque = Mathf.Infinity;
+        // frontLeft.steerAngle = 0;
+        // frontRight.steerAngle = 0;
+        // currentBrakeForce = breakingForce;
+    }
+}
